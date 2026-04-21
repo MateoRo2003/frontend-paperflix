9977f552a8f9f0027336b6e857af1a067187a348
Mateo Rojas <mateorojasarce2003@gmail.com>
2026-04-21 02:15:42 -0300

    wize

--- src/app/[subject]/page.tsx
+++ src/app/[subject]/page.tsx
@@ -362,7 +362,7 @@ export default function SubjectPage() {
           )}
         </div>
       ) : (
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {resources.map(r => (
             <ResourceCard key={r.id} resource={r} onClick={() => setSelected(r)} />
           ))}
